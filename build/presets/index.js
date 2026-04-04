e34148b835c8de6b642e93f8e75279d2f3ad3c52
Wild Mouse <3snowp7im@gmail.com>
2026-04-04 10:59:48 -0500

    Remove chosenPreset envvar

--- build/presets/index.js
+++ build/presets/index.js
@@ -1,33 +1,30 @@
 (function(self) {
 
-  let exports
-  if (self) {    
-    exports = self.sotnRando.presets
-  } else {
-    // Load the basic presets. All preset used as part of inheritance must be here.
-    exports = [
-      require('./casual'),
-      require('./safe'),
-      require('./adventure'),
-      require('./nimble'),
-      require('./open'),
-      require('./nimble-lite'),
-    ]
-    // We will use this to know which preset we loaded already. If you add any new inheritance preset, add it here.
-    let loadedPresets = ["casual", "safe", "adventure", "nimble", "open", "nimble-lite"]  
-    // Then only load the file of the specified preset
-    if(!(process.env.chosenPreset in loadedPresets) && process.env.chosenPreset !== undefined){   
-      let presetToLoad = process.env.chosenPreset
-      loadPreset = require(`./${presetToLoad}`)      
-      exports.push(loadPreset)
-    }
-  }
-
   if (self) {    
     self.sotnRando = Object.assign(self.sotnRando || {}, {
-      presets: exports,
+      presets: self.sotnRando.presets,
     })
   } else {
-    module.exports = exports
+    // We will use this to know which preset we loaded already.
+    // If you add any new inheritance preset, add it here.
+    const loadedPresets = [
+      'casual',
+      'safe',
+      'adventure',
+      'nimble',
+      'open',
+      'nimble-lite',
+    ]
+    module.exports = function(preset) {
+      // Load the basic presets.
+      const presets = loadedPresets.map(function(preset) {
+        return require(`./${preset}`)
+      })
+      // Then only load the file of the specified preset.
+      if (preset !== undefined && !(preset in loadedPresets)) {
+        presets.push(require(`./${preset}`))
+      }
+      return presets
+    }
   }
 })(typeof(self) !== 'undefined' ? self : null)
